ea1bc59cfd9a325b0ca663c01b41bfda96a619c2
yowtzu.lim@gmail.com <yowtzu.lim@gmail.com>
2014-07-08 08:34:52 +0100

    code for plotting

--- code/StochasticProcess.py
+++ code/StochasticProcess.py
@@ -1,19 +1,24 @@
 import numpy as np
 import scipy as sp
+import matplotlib.pyplot as plt
+
 from scipy import stats
+from collections import Iterable
 
 class StochasticProcess:
-    def __init__(self):
+    def __init__(self, seed=10):
         """default parameter is set to be mean = 1.0 2.0, and cov=identity(2)"""
-        self.mu = np.array([1.0, 2.0])
-        self.sigma = np.identity(2)
-        self.y_mu = np.array([3.0, 4.0])
-        self.y_sigma = np.identity(2)
+        np.random.seed(seed)
+        self.mu = np.array([0.0])
+        self.sigma = np.identity(1)
+        self.y_mu = np.array([0.0])
+        self.y_sigma = np.identity(1)
         self.x_dim = len(self.mu)
         self.y_dim = len(self.y_mu)
 
     def sample_initial(self):
-        return stats.multivariate_normal(self.mu, self.sigma).rvs()
+        return self.mu
+        # return stats.multivariate_normal(self.mu, self.sigma).rvs()
 
     def initial_density(self, x):
         return stats.multivariate_normal(self.mu, self.sigma).pdf(x)
@@ -29,13 +34,14 @@ class StochasticProcess:
   
     def observation_density(self, y, x):
         return stats.multivariate_normal(x+self.y_mu, self.y_sigma).pdf(y)
-
-    def simulate(self, T=100, seed=12345):
+    
+    def simulate(self, T=100):
+        self.T = T
         self.xs= np.zeros(shape=[T, self.x_dim])
         self.ys= np.zeros(shape=[T, self.y_dim])
 
         # initialise x[0] and y[0]                                           
-        self.xs[0] = self.sample_initial()
+        self.xs[0] = 0
         self.ys[0] = self.sample_observation(self.xs[0])
         
         for t in range(1,T):
@@ -44,10 +50,52 @@ class StochasticProcess:
 
     def plot(self):
         """ plot each dimension on different windows/lines """
-        pylab.figure()
-        pylab.plot(self.ys,'k+',label='noisy measurements (ys)')
-        pylab.plot(self.xs,'b-',label='hideen state (xs) values')
-        pylab.legend()
-        pylab.xlabel('Iteration')
-        pylab.ylabel('Observation')
+        fig, axes = plt.subplots(nrows=1, ncols=self.x_dim)
+        y_min = np.min(self.xs)
+        y_max = np.max(self.xs)
 
+        if not isinstance(axes, Iterable):
+            axes = [axes]
+            
+        for idx, ax in enumerate(axes):
+            ax.plot(range(self.T), self.xs[:,idx], 'r')
+            ax.grid(True)
+            ax.set_ylim(y_min, y_max)
+            ax.set_xlabel('T')
+            ax.set_ylabel('x')
+            ax.set_title('hidden states')
+        fig.tight_layout()
+
+        fig, axes = plt.subplots(nrows=1, ncols=self.y_dim)
+        y_min = np.min(self.ys)
+        y_max = np.max(self.ys)
+        
+        if not isinstance(axes, Iterable):
+            axes = [axes]
+            
+        for idx, ax in enumerate(axes):
+            ax.plot(range(self.T), self.ys[:,idx], 'r')
+            ax.grid(True)
+            ax.set_ylim(y_min, y_max)
+            ax.set_xlabel('T')
+            ax.set_ylabel('y')
+            ax.set_title('observation states')
+        fig.tight_layout()
+
+x = StochasticProcess()
+x.simulate()
+x.plot()
+
+N_SIM = 10000
+res = np.zeros(shape=[N_SIM, 100, 1])
+for i in range(N_SIM):
+    x = StochasticProcess(100+i)
+    x.simulate()
+    res[i] = x.xs
+
+x.mean = np.zeros(shape=100)
+x.var = np.zeros(shape=100)
+for i in range(100):
+    x.mean[i] = np.mean(res[:,i, :])
+    x.var[i] = np.var(res[:,i,:])
+x.mean
